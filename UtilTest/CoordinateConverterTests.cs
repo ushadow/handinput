29020be738db7183e8fd493de086fcfe9404abd6
Ying Yin <clevershadow@gmail.com>
2014-01-10 13:39:00 -0500

    Adding unit test to Util.

---
+++ UtilTest/CoordinateConverterTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Kinect;
+using HandInput.Util;
+ 
+namespace HandInput.UtilTest {
+  [TestClass]
+  public class CoordinateConverterTests {
+    CoordinateConverter converter;
+    [TestInitialize()]
+    public void Initialize() {
+      var mapper = new CoordinateMapper(Properties.Resources.ColorToDepthRelationalParameters);
+      converter = new CoordinateConverter(mapper, HandInputParams.ColorImageFormat, 
+                                          HandInputParams.DepthImageFormat);
+    }
+    
+    [TestMethod]
+    public void TestMapDepthPointToColorRect() {
+      var colorPoint = converter.MapDepthPointToColorPoint(230, 480, 500);
+      Console.WriteLine(colorPoint);
+      Assert.AreEqual(colorPoint.X, 0);
+    }
+  }
+}
